0749d8e14b850ac9b82c9738b87239beaa69c82a
Hugo Maia <maiahugom123@gmail.com>
2022-11-12 12:43:38 -0300

    Adicionando api e responsividade aos nomes

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const limit = 9
+const offset = 0
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+              <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+              </ol>
+
+              <img src="./assets/svg/poke-bulbasaur.svg" alt="${pokemon.name}" />
+            </div>
+          </li>`
+}
+
+const pokemonList = document.getElementById("pokemonList")
+
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i]
+      pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+  })
+  .catch((error) => error())
